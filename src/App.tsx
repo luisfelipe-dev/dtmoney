64f6ceec9f500bd364bcd01d6accecb03e3300b1
Luís <luis.oliveira@stoom.com.br>
2021-03-30 17:49:01 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Title = styled.h1`
+	color: red;
+`;
+
+export function App() {
+	return (
+		<div className="App">
+			<Title>Hello World</Title>
+		</div>
+	);
 }
-
-export default App;
